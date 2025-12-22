975b219e0616baf2dd107af6b852c28402a9b435
thisiskenneth <kacortez@espe.edu.ec>
2025-12-22 01:39:36 -0500

    websocket correction

--- BACKEND/BIBLIOTECA/src/main/java/com/grupobb/biblioteca/repository/ExampleRepository.java
+++
@@ -1,5 +0,0 @@
-package com.grupobb.biblioteca.repository;
-
-// Eliminado: repositorio de ejemplo no usado en el proyecto.
-// Este archivo queda como marcador; se puede borrar manualmente si se desea.
-
